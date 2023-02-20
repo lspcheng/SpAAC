e4ff5c2269576ce925dd391d6435b0f9688a0064
Lauretta S.P. Cheng <53014890+lspcheng@users.noreply.github.com>
2023-02-20 18:16:05 -0500

    Add materials

---
+++ 02-materials/01-code/mk_spkr_dir.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# To run (macOS): sh mk_spkr_dir.sh speaker
+
+# hard path
+path="/Users/laurettacheng/Documents/Projects/SpAAC/02-materials/02-stimuli/03-n2/01-auditory-stim/03-recordings"
+
+# input speaker code
+speaker=$1 # e.g., S07
+
+# make audio dirs
+mkdir -p "$path/$speaker/1_audio/1_original" "$path/$speaker/1_audio/2_processed" && cat <<EOF > "$path/$speaker/1_audio/2_processed/records.txt"
+# Audacity settings
+
+noise reduction =
+sensitivity =
+frequency smoothing =
+EOF
+
+# make textgird dirs
+mkdir -p "$path/$speaker/2_textgrid/1_original" "$path/$speaker/2_textgrid/2_manual/temp" && cat <<EOF > "$path/$speaker/2_textgrid/1_original/records.txt"
+# Praat settings
+
+silence threshold =
+silence duration (min) =
+sounding duration (min) =
+EOF
